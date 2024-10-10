b5778d5607095d97cd17a60ed5dce5e28e69d3ed
Patricio Leroux <pleroux@umich.edu>
2024-10-10 17:55:12 -0400

    initial commit

--- server/media/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Media
-from .models import Media, UserMediaInteraction
-
-# Register your models here.
-admin.site.register(Media)
-admin.site.register(UserMediaInteraction)
-
-
