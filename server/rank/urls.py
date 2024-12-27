9d86d5b4e30dca218be3e62b340cfdbd33576a20
Patricio Leroux <pleroux@umich.edu>
2024-12-26 20:48:11 -0800

    added test route

--- server/rank/urls.py
+++ server/rank/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import include, path
 
+def root_view(request):
+    return JsonResponse({"message": "Backend is running"})
+
 urlpatterns = [
+    path('', root_view),
     path('admin/', admin.site.urls),
     path('api/media/', include('media.urls')),
     path('api/auth/', include('users.urls')),  # Include the users app URLs here
